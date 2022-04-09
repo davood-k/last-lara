8c8477e7bba64986ce14b7f6fe863fbc5c015267
farhan\yazdan <davood.khodadadi@gmail.com>
2022-04-09 23:02:52 +0430

    project admin

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// roocket.ir/admin
+
+Route::namespace('Admin')->prefix('admin')->group(function (){
+    $this->get('/panel' , 'PanelController@index');
+
+    $this->resource('articles' , 'ArticleController');
+});
+//Route::resource('article')
